72ce72934775a4b912bec5dc8a4e8a5171279b37
Matheus Ramos <msilvaramos84@gmail.com>
2022-09-28 15:28:04 -0300

    Add encoding to read csv files correctly

--- occupations/occupations/api/find_occupation.py
+++ occupations/occupations/api/find_occupation.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
 
+# add requirements.txt
+
 def find(data):
-    df = pd.read_csv(os.path.join(os.getcwd(), 'occupations/api/data/CBO2002 - Grande Grupo.csv'), delimiter=';')
+    df = pd.read_csv(os.path.join(os.getcwd(), 'occupations/api/data/CBO2002 - Grande Grupo.csv'), delimiter=';', encoding='ISO-8859-9')
     data['name'] = df["TITULO"][0].strip()
     return data
